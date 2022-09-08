3e3e15e951e6090423b3444b36ae85c1d96cd411
Nathan Lima <nathanlima523@hotmail.com>
2022-09-08 18:02:25 -0300

    Primeiro commit

---
+++ src/main/java/br/com/elo7/desafioprogramacao/DesafioprogramacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.elo7.desafioprogramacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioprogramacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioprogramacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/elo7/desafioprogramacao/DesafioprogramacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.elo7.desafioprogramacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioprogramacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
